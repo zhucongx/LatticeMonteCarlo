de21cf8d06cb9bd020ff5b56996f9a2be61b4d25
Zhucong Xi <zhucongx@umich.edu>
2022-05-12 16:53:51 -0400

    Update restart.py

--- script/restart.py
+++ script/restart.py
@@ -1,32 +1,69 @@
 from os.path import exists
+from os import rename
+import os
 
-for T in [300, 600, 900]:
-    with open(f'{T}K/kmc_log.txt', 'r') as f1:
-        lines = f1.readlines()
+
+def reverse_readline(filename, buf_size=8192):
+    """A generator that returns the lines of a file in reverse order"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # The first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first
+                if buffer[-1] != '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
+
+
+for T in [300, 400, 500, 600, 700, 800]:
+    if (exists(f'{T}K/kmc_log.txt')):
+        rename(f'{T}K/kmc_log.txt', f'{T}K/kmc_log_backup.txt')
     last_i = None
     last_step = None
     last_time = None
     last_energy = None
-    for i, line in enumerate(lines):
+
+    for i, line in enumerate(reverse_readline(f'{T}K/kmc_log_backup.txt')):
         line = line.split()
-        if exists(f'{T}K/{line[0]}.cfg'):
+        if (exists(f'{T}K/{line[0]}.cfg')):
             last_i = i
             last_step = line[0]
             last_time = line[1]
             last_energy = line[2]
-
-    print(f"{T}K, {last_step}, {last_time}, {last_energy}")
-
-    with open(f'{T}K/kmc_log_backup.txt', 'w') as f2:
-        f2.write(''.join(lines))
-        f2.flush()
-    with open(f'{T}K/kmc_log.txt', 'w') as f3:
-        f3.write(''.join(lines[:last_i + 1]))
-        f3.flush()
+            break
+    print(f"{T}K, {last_i}, {last_step}, {last_time}, {last_energy}")
+    with open(f'{T}K/kmc_log_backup.txt', 'r') as f1, open(f'{T}K/kmc_log.txt', 'w') as f2:
+        for i, line in enumerate(f1):
+            f2.write(line)
+            step = line.split()[0]
+            if step == last_step:
+                f2.flush()
+                break
     with open(f'{T}K/kmc.param', 'w') as f4:
         f4.write(f"config_filename {last_step}.cfg\n")
-        f4.write(f"json_parameters_filename kmc_parameters_state.json\n")
-        f4.write(f"log_dump_steps 1000\n")
+        f4.write(f"json_parameters_filename kmc_parameters_quartic.json\n")
+        f4.write(f"log_dump_steps 1\n")
         f4.write(f"config_dump_steps 100000\n")
         f4.write(f"maximum_number 10000000000\n")
         f4.write(f"temperature {T}\n")
@@ -35,3 +72,4 @@ for T in [300, 600, 900]:
         f4.write(f"restart_energy {last_energy}\n")
         f4.write(f"restart_time {last_time}\n")
         f4.flush()
+    print(f"Done with {T}K")
