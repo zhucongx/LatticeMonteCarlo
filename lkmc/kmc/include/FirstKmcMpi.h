e02172c2a8e1ae20b4555315c36582f6cb57bdf5
Zhucong Xi <zhucongx@umich.edu>
2022-05-13 00:50:57 -0400

    first kmc

---
+++ lkmc/kmc/include/FirstKmcMpi.h
@@ -0,0 +1,62 @@
+#ifndef LKMC_LKMC_KMC_INCLUDE_FIRSTKMCMPI_H_
+#define LKMC_LKMC_KMC_INCLUDE_FIRSTKMCMPI_H_
+#include <random>
+#include <mpi.h>
+#include "EnergyPredictorSymmetry.h"
+#include "EnergyPredictorStateLru.h"
+#include "EnergyPredictorQuarticLru.h"
+#include "JumpEvent.h"
+namespace kmc {
+class FirstKmcMpi {
+  public:
+    FirstKmcMpi(cfg::Config config,
+                unsigned long long int log_dump_steps,
+                unsigned long long int config_dump_steps,
+                unsigned long long int maximum_number,
+                double temperature,
+                const std::set<Element> &type_set,
+                unsigned long long int restart_steps,
+                double restart_energy,
+                double restart_time,
+                const std::string &json_coefficients_filename);
+    virtual ~FirstKmcMpi();
+    virtual void Simulate();
+  protected:
+    inline void Dump(std::ofstream &ofs);
+    void BuildEventListParallel();
+    [[nodiscard]] size_t SelectEvent() const;
+
+    // constants
+    static constexpr size_t kEventListSize = constants::kNumFirstNearestNeighbors;
+
+    // simulation parameters
+    cfg::Config config_;
+    const unsigned long long int log_dump_steps_;
+    const unsigned long long int config_dump_steps_;
+    const unsigned long long int maximum_number_;
+    const double beta_;
+
+    // simulation statistics
+    unsigned long long int steps_;
+    double energy_;
+    double time_;
+    const size_t vacancy_index_;
+
+    // simulation variables
+    double one_step_barrier_{0.0};
+    double one_step_energy_change_{0.0};
+    double one_step_time_change_{0.0};
+    Element migrating_element_{};
+
+    // helpful properties
+    double total_rate_{0.0};
+    int mpi_rank_{-1};
+    std::pair<size_t, size_t> atom_id_jump_pair_;
+
+    std::vector<JumpEvent> event_list_{};
+    const pred::EnergyPredictorQuarticLru energy_predictor_;
+    mutable std::mt19937_64 generator_;
+};
+} // namespace kmc
+
+#endif //LKMC_LKMC_KMC_INCLUDE_FIRSTKMCMPI_H_
---
+++ lkmc/kmc/src/FirstKmcMpi.cpp
@@ -0,0 +1,121 @@
+#include "FirstKmcMpi.h"
+namespace kmc {
+FirstKmcMpi::FirstKmcMpi(cfg::Config config,
+                         unsigned long long int log_dump_steps,
+                         unsigned long long int config_dump_steps,
+                         unsigned long long int maximum_number,
+                         double temperature,
+                         const std::set<Element> &type_set,
+                         unsigned long long int restart_steps,
+                         double restart_energy,
+                         double restart_time,
+                         const std::string &json_coefficients_filename)
+    : config_(std::move(config)),
+      log_dump_steps_(log_dump_steps),
+      config_dump_steps_(config_dump_steps),
+      maximum_number_(maximum_number),
+      beta_(1 / kBoltzmannConstant / temperature),
+      steps_(restart_steps),
+      energy_(restart_energy),
+      time_(restart_time),
+      vacancy_index_(cfg::GetVacancyAtomIndex(config_)),
+      energy_predictor_(json_coefficients_filename,
+                        config_, type_set, 100000),
+      generator_(static_cast<unsigned long long int>(
+                     std::chrono::system_clock::now().time_since_epoch().count())) {
+  event_list_.resize(kEventListSize);
+  MPI_Init(nullptr, nullptr);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank_);
+  int mpi_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+  if (mpi_size != kEventListSize) {
+    std::cout << "Must use " << kEventListSize << " precesses. Terminating.\n";
+    MPI_Finalize();
+    exit(0);
+  }
+  if (mpi_rank_ == 0) {
+    std::cout << "Using " << mpi_size << " processes." << std::endl;
+  }
+}
+FirstKmcMpi::~FirstKmcMpi() {
+  MPI_Finalize();
+}
+void FirstKmcMpi::Dump(std::ofstream &ofs) {
+  if (steps_ % log_dump_steps_ == 0) {
+    ofs << steps_ << '\t' << time_ << '\t' << energy_ << '\t' << one_step_barrier_ << '\t'
+        << one_step_energy_change_ << '\t' << migrating_element_.GetString() << std::endl;
+  }
+  if (steps_ % config_dump_steps_ == 0) {
+    config_.WriteCfg(std::to_string(steps_) + ".cfg", true);
+  }
+}
+void FirstKmcMpi::BuildEventListParallel() {
+  total_rate_ = 0;
+  const auto neighbor_index =
+      config_.GetFirstNeighborsAtomIdVectorOfAtom(vacancy_index_)[static_cast<size_t>(mpi_rank_)];
+  JumpEvent event
+      ({vacancy_index_, neighbor_index},
+       energy_predictor_.GetBarrierAndDiffFromAtomIdPair(config_,
+                                                         {vacancy_index_, neighbor_index}),
+       beta_);
+  const double this_rate = event.GetForwardRate();
+
+  MPI_Allgather(&event,
+                sizeof(JumpEvent),
+                MPI_BYTE,
+                event_list_.data(),
+                sizeof(JumpEvent),
+                MPI_BYTE,
+                MPI_COMM_WORLD);
+  MPI_Allreduce(&this_rate, &total_rate_, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+}
+size_t FirstKmcMpi::SelectEvent() const {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+  const double random_number = distribution(generator_);
+  auto it = std::lower_bound(event_list_.begin(),
+                             event_list_.end(),
+                             random_number,
+                             [](const auto &lhs, double value) {
+                               return lhs.GetCumulativeProvability() < value;
+                             });
+  // If not find (maybe generated 1), which rarely happens, returns the last event
+  if (it == event_list_.cend()) {
+    it--;
+  }
+  return static_cast<size_t>(std::distance(event_list_.begin(), it));
+}
+void FirstKmcMpi::Simulate() {
+  std::ofstream ofs("kmc_log.txt", std::ofstream::out | std::ofstream::app);
+  if (mpi_rank_ == 0) {
+    ofs << "steps\ttime\tenergy\tEa\tdE\ttype\n";
+    ofs.precision(8);
+  }
+
+  while (steps_ < maximum_number_) {
+    if (mpi_rank_ == 0) {
+      Dump(ofs);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    BuildEventListParallel();
+    JumpEvent selected_event;
+    if (mpi_rank_ == 0) {
+      selected_event = event_list_[SelectEvent()];
+    }
+    MPI_Bcast(&selected_event, sizeof(JumpEvent), MPI_BYTE, 0, MPI_COMM_WORLD);
+
+    atom_id_jump_pair_ = selected_event.GetAtomIdJumpPair();
+
+    // if (!CheckAndSolveEquilibrium(ofs)) {
+    // update time and energy
+    time_ += one_step_time_change_;
+    one_step_energy_change_ = selected_event.GetEnergyChange();
+    energy_ += one_step_energy_change_;
+    one_step_barrier_ = selected_event.GetForwardBarrier();
+    migrating_element_ = config_.GetAtomVector().at(atom_id_jump_pair_.second).GetElement();
+    config_.AtomJump(atom_id_jump_pair_);
+    // }
+    ++steps_;
+  }
+}
+} // namespace kmc
