242b98073215d6e33493d299dd7e7f3acd61ab81
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2022-11-02 16:43:43 -0400

    1

---
+++ lkmc/kmc/include/ChainKmcOmpi.h
@@ -0,0 +1,16 @@
+//
+// Created by Zhucong Xi on 11/2/22.
+//
+
+#ifndef LKMC_LKMC_KMC_INCLUDE_CHAINKMCOMPI_H_
+#define LKMC_LKMC_KMC_INCLUDE_CHAINKMCOMPI_H_
+
+namespace kmc {
+
+class ChainKmcOmpi {
+
+};
+
+} // kmc
+
+#endif //LKMC_LKMC_KMC_INCLUDE_CHAINKMCOMPI_H_
---
+++ lkmc/kmc/src/ChainKmcOmpi.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Zhucong Xi on 11/2/22.
+//
+
+#include "ChainKmcOmpi.h"
+
+namespace kmc {
+} // kmc
