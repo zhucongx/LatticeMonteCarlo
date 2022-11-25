067e227042c4a511c115bbd1492e0a990390683b
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2022-11-25 02:17:13 -0500

    Update ThermodynamicAveraging.cpp

--- lkmc/mc/src/ThermodynamicAveraging.cpp
+++ lkmc/mc/src/ThermodynamicAveraging.cpp
@@ -6,12 +6,12 @@ mc::ThermodynamicAveraging::ThermodynamicAveraging(size_t size)
     : size_(size) {
 }
 void ThermodynamicAveraging::AddEnergy(double value) {
-    if (energy_list_.size() == size_) {
-        sum_ -= energy_list_.front();
-        energy_list_.pop_front();
-    }
-    energy_list_.push_back(value);
-    sum_ += value;
+  if (energy_list_.size() == size_) {
+    sum_ -= energy_list_.front();
+    energy_list_.pop_front();
+  }
+  energy_list_.push_back(value);
+  sum_ += value;
 }
 double ThermodynamicAveraging::GetAverage() const {
   if (energy_list_.empty()) {
@@ -23,9 +23,10 @@ double ThermodynamicAveraging::GetThermodynamicAverage(double temperature) const
   const auto average = GetAverage();
   double partition = 0.0;
   double thermodynamic_average_energy = 0.0;
+  double beta = 1.0 / (constants::kBoltzmannConstant * temperature);
   for (auto energy: energy_list_) {
     energy -= average;
-    const double exp_value = std::exp(-energy / (constants::kBoltzmannConstant * temperature));
+    const double exp_value = std::exp(-energy * beta);
     thermodynamic_average_energy += energy * exp_value;
     partition += exp_value;
   }
