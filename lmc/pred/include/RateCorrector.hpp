f2bad1701a9455cad241407f7fe0497a0e9c3118
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-01-18 17:07:37 -0500

    Create RateCorrector.hpp

---
+++ lmc/pred/include/RateCorrector.hpp
@@ -0,0 +1,31 @@
+#ifndef LMC_LMC_PRED_INCLUDE_RATECORRECTOR_HPP_
+#define LMC_LMC_PRED_INCLUDE_RATECORRECTOR_HPP_
+#include <cmath>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+#include "Constants.hpp"
+namespace pred {
+
+class RateCorrector {
+
+  public:
+    RateCorrector(double vacancy_concentration, double solute_concentration)
+        : vacancy_concentration_(vacancy_concentration),
+          solute_concentration_(solute_concentration) {}
+    inline double GetTimeCorrectionFactor(double temperature) const {
+      return vacancy_concentration_ / GetCorrectVacancyConcentration(temperature)
+          / (1 - 13 * solute_concentration_);
+    };
+  private:
+    static inline double GetCorrectVacancyConcentration(double temperature) {
+      return 1.64 * std::exp(-(0.66 / constants::kBoltzmann / temperature - 0.7));
+    }
+    double vacancy_concentration_;
+    double solute_concentration_;
+};
+
+} // pred
+
+#endif //LMC_LMC_PRED_INCLUDE_RATECORRECTOR_HPP_
