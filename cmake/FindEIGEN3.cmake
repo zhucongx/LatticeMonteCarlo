ccff170e7e223d06ccfe6864e36e12c4b16a8e8b
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-02-22 15:18:20 -0500

    Update FindEIGEN3.cmake

--- cmake/FindEIGEN3.cmake
+++ cmake/FindEIGEN3.cmake
@@ -1,9 +1,9 @@
 set(FIND_EIGEN3_PATHS ~/.local /usr /usr/local /usr/share ${C_PATH})
 find_path(EIGEN3_INCLUDE_DIRS
         NAMES eigen3
-        PATH_SUFFIXES include
+#        PATH_SUFFIXES include
         PATHS ${FIND_EIGEN3_PATHS})
 
-find_library(EIGEN3_LIBRARIES
-        NAMES eigen3
-        HINTS ${FIND_EIGEN3_PATHS})
+#find_library(EIGEN3_LIBRARIES
+#        NAMES eigen3
+#        HINTS ${FIND_EIGEN3_PATHS})
