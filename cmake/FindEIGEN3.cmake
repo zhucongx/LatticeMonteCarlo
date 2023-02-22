fbef3888b83e4702df652ad1b6b8c8d9a1ceebb6
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-02-22 14:52:00 -0500

    Revert "Delete FindEIGEN3.cmake"
    This reverts commit 8f9772c69080ee77c7072621d5683b4e33d6280d.

---
+++ cmake/FindEIGEN3.cmake
@@ -0,0 +1,9 @@
+set(FIND_EIGEN3_PATHS ~/.local /usr /usr/local /usr/share)
+find_path(EIGEN3_INCLUDE_DIRS
+        NAMES eigen3
+        PATH_SUFFIXES include
+        PATHS ${FIND_EIGEN3_PATHS})
+
+find_library(EIGEN3_LIBRARIES
+        NAMES eigen3
+        HINTS ${FIND_EIGEN3_PATHS})
