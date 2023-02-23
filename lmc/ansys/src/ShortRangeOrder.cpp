25907efe0e0534de600a631ab9860da103435191
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-02-23 17:54:43 -0500

    sro

--- lmc/ansys/src/ShortRangeOrder.cpp
+++ lmc/ansys/src/ShortRangeOrder.cpp
@@ -3,13 +3,85 @@
 #include <utility>
 
 namespace ansys {
-ShortRangeOrder::ShortRangeOrder(cfg::Config config, std::set<Element> element_set)
-    : config_(std::move(config)), element_set_(std::move(element_set)) {}
-std::map<std::string, double> ShortRangeOrder::FindWarrenCowley(const size_t shell_number) const {
-  std::map<std::string, double> res;
+ShortRangeOrder::ShortRangeOrder(const cfg::Config &config, const std::set<Element> &element_set)
+    : config_(config), element_set_(element_set) {}
+std::map<std::string, double> ShortRangeOrder::FindWarrenCowleyCluster(
+    const size_t shell_number, const std::vector<size_t> &cluster_atom_id_list) const {
+  std::map<std::string, double> warren_cowley;
   for (const auto &element1: element_set_) {
     for (const auto &element2: element_set_) {
-      res[element1.GetString() + "-" + element2.GetString()] = 0;
+      warren_cowley[element1.GetString() + "-" + element2.GetString()] = 0;
+    }
+  }
+  std::map<Element, std::vector<size_t> > element_list_map{};
+  std::map<Element, double> concentration;
+  std::map<Element, double> count;
+  for (const auto &element: element_set_) {
+    count[element] = 0;
+    element_list_map[element] = {};
+  }
+  size_t num_atoms = 0;
+  for (const auto &atom_id: cluster_atom_id_list) {
+    auto element = config_.GetAtomVector()[atom_id].GetElement();
+    if (element == ElementName::X) { continue; }
+    count.at(element)++;
+    element_list_map.at(element).push_back(atom_id);
+    num_atoms++;
+  }
+  for (const auto type: element_set_) {
+    concentration[type] = count.at(type) / static_cast<double>(num_atoms);
+  }
+
+  size_t num_bonds;
+  switch (shell_number) {
+    case 1:num_bonds = constants::kNumFirstNearestNeighbors;
+      break;
+    case 2:num_bonds = constants::kNumSecondNearestNeighbors;
+      break;
+    case 3:num_bonds = constants::kNumThirdNearestNeighbors;
+      break;
+    default:throw std::invalid_argument("Unknown shell number: " + std::to_string(shell_number));
+  }
+  for (const auto &element1: element_set_) {
+    size_t num_all_bonds = element_list_map.at(element1).size() * num_bonds;
+    std::map<Element, size_t> ct_this_pair_map{};
+    for (const auto &element2: element_set_) {
+      ct_this_pair_map[element2] = 0;
+    }
+    for (const auto &atom_id1: element_list_map.at(element1)) {
+      std::vector<size_t> neighbor_list;
+      switch (shell_number) {
+        case 1:neighbor_list = config_.GetFirstNeighborsAtomIdVectorOfAtom(atom_id1);
+          break;
+        case 2:neighbor_list = config_.GetSecondNeighborsAtomIdVectorOfAtom(atom_id1);
+          break;
+        case 3:neighbor_list = config_.GetThirdNeighborsAtomIdVectorOfAtom(atom_id1);
+          break;
+        default:;
+      }
+      for (const auto &atom_id2: neighbor_list) {
+        const auto &this_element = config_.GetElementAtAtomId(atom_id2);
+        if (this_element == ElementName::X) {
+          continue;
+        }
+        ct_this_pair_map[this_element]++;
+      }
+    }
+    for (auto [element2, ct_this_pair]: ct_this_pair_map) {
+      std::string key = element1.GetString() + "-" + element2.GetString();
+      double pij = static_cast<double>(ct_this_pair) / static_cast<double>(num_all_bonds);
+      warren_cowley[key] = (pij - concentration[element2]) /
+          (static_cast<double>(element1 == element2) - concentration[element2]);
+      // res[key] = 1 - (pij / concentration[element2]);
+    }
+  }
+  return warren_cowley;
+}
+std::map<std::string, double> ShortRangeOrder::FindWarrenCowley(const size_t shell_number) const {
+  std::map<std::string, double> warren_cowley;
+  for (const auto &element1: element_set_) {
+    for (const auto &element2: element_set_) {
+      warren_cowley[element1.GetString() + "-" + element2.GetString()] = 0;
     }
   }
   const auto &element_list_map = config_.GetElementAtomIdVectorMap();
@@ -58,12 +130,12 @@ std::map<std::string, double> ShortRangeOrder::FindWarrenCowley(const size_t she
     for (auto [element2, ct_this_pair]: ct_this_pair_map) {
       std::string key = element1.GetString() + "-" + element2.GetString();
       double pij = static_cast<double>(ct_this_pair) / static_cast<double>(num_all_bonds);
-      res[key] = (pij - concentration[element2]) /
+      warren_cowley[key] = (pij - concentration[element2]) /
           (static_cast<double>(element1 == element2) - concentration[element2]);
       // res[key] = 1 - (pij / concentration[element2]);
     }
   }
-  return res;
+  return warren_cowley;
 }
 // std::map<std::string, double> ShortRangeOrder::FindWarrenCowley(size_t shell_number) const {
 //   std::map<std::string, double> res;
