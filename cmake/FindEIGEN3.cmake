137c513cb0b194acd7103ea89ca8a0dd02f62366
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-02-23 14:34:45 -0500

    Delete FindEIGEN3.cmake

--- cmake/FindEIGEN3.cmake
+++
@@ -1,9 +0,0 @@
-set(FIND_EIGEN3_PATHS ~/.local /usr /usr/local /usr/share ${C_PATH})
-find_path(EIGEN3_INCLUDE_DIRS
-        NAMES eigen3
-#        PATH_SUFFIXES include
-        PATHS ${FIND_EIGEN3_PATHS})
-message(STATUS "EIGEN3_INCLUDE_DIRS: ${EIGEN3_INCLUDE_DIRS}")
-#find_library(EIGEN3_LIBRARIES
-#        NAMES eigen3
-#        HINTS ${FIND_EIGEN3_PATHS})
