f2500ee1c15caff1096e5758be7b5c04faeda0f8
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-06-30 15:11:59 -0400

    Update FindEigen3.cmake

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -5,13 +5,17 @@
 # to require version 3.1.2 or newer of Eigen3.
 #
 # Once done this will define
-#
+
 #  EIGEN3_FOUND - system has eigen lib with correct version
-#  EIGEN3_INCLUDE_DIRSS - the eigen include directory
+#  EIGEN3_INCLUDE_DIRS - the eigen include directory
 #  EIGEN3_VERSION - eigen version
 #
+#  and the following imported target:
+
+#  Eigen3::Eigen - The header-only Eigen library
+#
 # This module reads hints about search locations from
-# the following enviroment variables:
+# the following environment variables:
 #
 # EIGEN3_ROOT
 # EIGEN3_ROOT_DIR
@@ -20,19 +24,19 @@
 # Copyright (c) 2008, 2009 Gael Guennebaud, <g.gael@free.fr>
 # Copyright (c) 2009 Benoit Jacob <jacob.benoit.1@gmail.com>
 # Redistribution and use is allowed according to the terms of the 2-clause BSD license.
-if(NOT Eigen3_FIND_VERSION)
-    if(NOT Eigen3_FIND_VERSION_MAJOR)
+if (NOT Eigen3_FIND_VERSION)
+    if (NOT Eigen3_FIND_VERSION_MAJOR)
         set(Eigen3_FIND_VERSION_MAJOR 2)
-    endif(NOT Eigen3_FIND_VERSION_MAJOR)
-    if(NOT Eigen3_FIND_VERSION_MINOR)
+    endif (NOT Eigen3_FIND_VERSION_MAJOR)
+    if (NOT Eigen3_FIND_VERSION_MINOR)
         set(Eigen3_FIND_VERSION_MINOR 91)
-    endif(NOT Eigen3_FIND_VERSION_MINOR)
-    if(NOT Eigen3_FIND_VERSION_PATCH)
+    endif (NOT Eigen3_FIND_VERSION_MINOR)
+    if (NOT Eigen3_FIND_VERSION_PATCH)
         set(Eigen3_FIND_VERSION_PATCH 0)
-    endif(NOT Eigen3_FIND_VERSION_PATCH)
+    endif (NOT Eigen3_FIND_VERSION_PATCH)
 
     set(Eigen3_FIND_VERSION "${Eigen3_FIND_VERSION_MAJOR}.${Eigen3_FIND_VERSION_MINOR}.${Eigen3_FIND_VERSION_PATCH}")
-endif(NOT Eigen3_FIND_VERSION)
+endif (NOT Eigen3_FIND_VERSION)
 
 macro(_eigen3_check_version)
     file(READ "${EIGEN3_INCLUDE_DIRS}/Eigen/src/Core/util/Macros.h" _eigen3_version_header)
@@ -45,17 +49,17 @@ macro(_eigen3_check_version)
     set(EIGEN3_MINOR_VERSION "${CMAKE_MATCH_1}")
 
     set(EIGEN3_VERSION ${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.${EIGEN3_MINOR_VERSION})
-    if(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
+    if (${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
         set(EIGEN3_VERSION_OK FALSE)
-    else(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
+    else (${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
         set(EIGEN3_VERSION_OK TRUE)
-    endif(${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
+    endif (${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
 
-    if(NOT EIGEN3_VERSION_OK)
+    if (NOT EIGEN3_VERSION_OK)
 
         message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIRS}, "
                 "but at least version ${Eigen3_FIND_VERSION} is required")
-    endif(NOT EIGEN3_VERSION_OK)
+    endif (NOT EIGEN3_VERSION_OK)
 endmacro(_eigen3_check_version)
 
 if (EIGEN3_INCLUDE_DIRS)
@@ -71,13 +75,12 @@ else (EIGEN3_INCLUDE_DIRS)
     # the script will work as usual
     find_package(Eigen3 ${Eigen3_FIND_VERSION} NO_MODULE QUIET)
 
-    if(NOT EIGEN3_INCLUDE_DIRS)
+    if (NOT EIGEN3_INCLUDE_DIRS)
         find_path(EIGEN3_INCLUDE_DIRS NAMES signature_of_eigen3_matrix_library
                 HINTS
                 ENV EIGEN3_ROOT
                 ENV EIGEN3_ROOT_DIR
                 PATHS
-                ${CMAKE_INSTALL_PREFIX}/include
                 ${KDE4_INCLUDE_DIR}
                 ${CMAKE_INSTALL_PREFIX}/include/eigen3
                 ${CMAKE_INSTALL_PREFIX}/include
@@ -86,17 +89,25 @@ else (EIGEN3_INCLUDE_DIRS)
                 /usr/include/eigen3
                 /usr/include
                 /sw/pkgs/arc/eigen/eigen-3.4.0
+                ~/.local/include/eigen3
+                ~/.local/include/
                 PATH_SUFFIXES eigen3 eigen
                 )
-    endif(NOT EIGEN3_INCLUDE_DIRS)
+    endif (NOT EIGEN3_INCLUDE_DIRS)
 
-    if(EIGEN3_INCLUDE_DIRS)
+    if (EIGEN3_INCLUDE_DIRS)
         _eigen3_check_version()
-    endif(EIGEN3_INCLUDE_DIRS)
+    endif (EIGEN3_INCLUDE_DIRS)
 
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(Eigen3 DEFAULT_MSG EIGEN3_INCLUDE_DIRS EIGEN3_VERSION_OK)
 
     mark_as_advanced(EIGEN3_INCLUDE_DIRS)
 
-endif(EIGEN3_INCLUDE_DIRS)
+endif (EIGEN3_INCLUDE_DIRS)
+
+if (EIGEN3_FOUND AND NOT TARGET Eigen3::Eigen)
+    add_library(Eigen3::Eigen INTERFACE IMPORTED)
+    set_target_properties(Eigen3::Eigen PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIRS}")
+endif (EIGEN3_FOUND AND NOT TARGET Eigen3::Eigen)
