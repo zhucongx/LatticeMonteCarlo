dcdffe390a04cd705772a4e17306d18451c79df2
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-07-02 14:26:13 -0400

    Update Config.cpp

--- lmc/cfg/src/Config.cpp
+++ lmc/cfg/src/Config.cpp
@@ -3,7 +3,7 @@
  * @Author: Zhucong Xi                                                                            *
  * @Date: 1/16/20 3:55 AM                                                                         *
  * @Last Modified by: zhucongx                                                                    *
- * @Last Modified time: 6/30/23 3:33 PM                                                           *
+ * @Last Modified time: 7/1/23 1:06 AM                                                            *
  **************************************************************************************************/
 
 /*! \file  Config.cpp
@@ -20,9 +20,10 @@
 #include <boost/iostreams/filter/bzip2.hpp>
 
 Config::Config() = default;
-Config::Config(Matrix3d basis,
-               Matrix3Xd relative_position_matrix,
-               std::vector<Atom> atom_vector)
+
+Config::Config(Eigen::Matrix3d basis,
+               Eigen::Matrix3Xd relative_position_matrix,
+               std::vector<Element> atom_vector)
     : basis_(std::move(basis)),
       relative_position_matrix_(std::move(relative_position_matrix)),
       atom_vector_(std::move(atom_vector)) {
@@ -35,32 +36,41 @@ Config::Config(Matrix3d basis,
   cartesian_position_matrix_ = basis_ * relative_position_matrix_;
   for (size_t id = 0; id < atom_vector_.size(); ++id) {
     // id here is also lattice_id
-    auto atom_id = atom_vector_.at(id).GetId();
-    lattice_to_atom_hashmap_.emplace(id, atom_id);
-    atom_to_lattice_hashmap_.emplace(atom_id, id);
+    lattice_to_atom_hashmap_.emplace(id, id);
+    atom_to_lattice_hashmap_.emplace(id, id);
   }
 }
+
+Config::~Config() = default;
+
 size_t Config::GetNumAtoms() const {
   return atom_vector_.size();
 }
+
 size_t Config::GetNumSites() const {
   return static_cast<size_t>(relative_position_matrix_.cols());
 }
-const Matrix3d &Config::GetBasis() const {
+
+const Eigen::Matrix3d &Config::GetBasis() const {
   return basis_;
 }
+
 const std::vector<std::vector<std::vector<size_t> > > &Config::GetNeighborLists() const {
   return neighbor_lists_;
 }
-Vector3d Config::GetRelativePositionOfAtom(size_t atom_id) const {
+
+Eigen::Vector3d Config::GetRelativePositionOfAtom(size_t atom_id) const {
   return relative_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(atom_id)));
 }
-Vector3d Config::GetCartesianPositionOfAtom(size_t atom_id) const {
+
+Eigen::Vector3d Config::GetCartesianPositionOfAtom(size_t atom_id) const {
   return cartesian_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(atom_id)));
 }
+
 void Config::SetPeriodicBoundaryCondition(const std::array<bool, 3> &periodic_boundary_condition) {
   periodic_boundary_condition_ = periodic_boundary_condition;
 }
+
 void Config::AtomJump(const std::pair<size_t, size_t> &atom_id_jump_pair) {
   const auto [atom_id_lhs, atom_id_rhs] = atom_id_jump_pair;
   const auto lattice_id_lhs = atom_to_lattice_hashmap_.at(atom_id_lhs);
@@ -71,8 +81,46 @@ void Config::AtomJump(const std::pair<size_t, size_t> &atom_id_jump_pair) {
   lattice_to_atom_hashmap_.at(lattice_id_lhs) = atom_id_rhs;
   lattice_to_atom_hashmap_.at(lattice_id_rhs) = atom_id_lhs;
 }
-Vector3d Config::GetRelativeDistanceVectorLattice(size_t lattice_id1, size_t lattice_id2) const {
-  Vector3d relative_distance_vector = relative_position_matrix_.col(static_cast<int>(lattice_id2))
+
+void Config::ReassignLattice() {
+
+  std::vector<std::pair<Eigen::Vector3d, size_t>> new_lattice_id_vector(GetNumSites());
+  for (size_t i = 0; i < GetNumSites(); ++i) {
+    new_lattice_id_vector[i] = std::make_pair(relative_position_matrix_.col(static_cast<int>(i)), i);
+  }
+  std::sort(new_lattice_id_vector.begin(), new_lattice_id_vector.end(),
+            [](const auto &lhs, const auto &rhs) -> bool {
+              const double x_diff = lhs.first[0] - rhs.first[0];
+              if (x_diff < -constants::kEpsilon) { return true; }
+              if (x_diff > constants::kEpsilon) { return false; }
+              const double y_diff = lhs.first[1] - rhs.first[1];
+              if (y_diff < -constants::kEpsilon) { return true; }
+              if (y_diff > constants::kEpsilon) { return false; }
+              return lhs.first[2] < rhs.first[2] - constants::kEpsilon;
+            });
+
+  std::unordered_map<size_t, size_t> old_lattice_id_to_new;
+  for (size_t lattice_id = 0; lattice_id < GetNumAtoms(); ++lattice_id) {
+    old_lattice_id_to_new.emplace(new_lattice_id_vector.at(lattice_id).second, lattice_id);
+  }
+  std::unordered_map<size_t, size_t> new_lattice_to_atom_hashmap, new_atom_to_lattice_hashmap;
+  for (size_t atom_id = 0; atom_id < GetNumAtoms(); ++atom_id) {
+    auto old_lattice_id = atom_to_lattice_hashmap_.at(atom_id);
+    auto new_lattice_id = old_lattice_id_to_new.at(old_lattice_id);
+    new_lattice_to_atom_hashmap.emplace(new_lattice_id, atom_id);
+    new_atom_to_lattice_hashmap.emplace(atom_id, new_lattice_id);
+  }
+  // Copy sorted columns back into matrix
+  for (size_t i = 0; i < GetNumSites(); ++i) {
+    relative_position_matrix_.col(static_cast<int>(i)) = new_lattice_id_vector[i].first;
+  }
+  cartesian_position_matrix_ = basis_ * relative_position_matrix_;
+  lattice_to_atom_hashmap_ = new_lattice_to_atom_hashmap;
+  atom_to_lattice_hashmap_ = new_atom_to_lattice_hashmap;
+}
+
+Eigen::Vector3d Config::GetRelativeDistanceVectorLattice(size_t lattice_id1, size_t lattice_id2) const {
+  Eigen::Vector3d relative_distance_vector = relative_position_matrix_.col(static_cast<int>(lattice_id2))
       - relative_position_matrix_.col(static_cast<int>(lattice_id1));
   // periodic boundary conditions
   for (const size_t kDim : std::vector<size_t>{0, 1, 2}) {
@@ -87,6 +135,7 @@ Vector3d Config::GetRelativeDistanceVectorLattice(size_t lattice_id1, size_t lat
   }
   return relative_distance_vector;
 }
+
 void Config::UpdateNeighborList(std::vector<double> cutoffs) {
   cutoffs_ = std::move(cutoffs);
   std::sort(cutoffs_.begin(), cutoffs_.end());
@@ -118,8 +167,8 @@ void Config::UpdateNeighborList(std::vector<double> cutoffs) {
   // Create cells
   cells_ = std::vector<std::vector<size_t>>(static_cast<size_t>(num_cells_.prod()));
   for (size_t lattice_id = 0; lattice_id < GetNumSites(); ++lattice_id) {
-    const Vector3d relative_position = relative_position_matrix_.col(static_cast<int>(lattice_id));
-    Vector3i cell_pos = (num_cells_.cast<double>().array() * relative_position.array()).floor().cast<int>();
+    const Eigen::Vector3d relative_position = relative_position_matrix_.col(static_cast<int>(lattice_id));
+    Eigen::Vector3i cell_pos = (num_cells_.cast<double>().array() * relative_position.array()).floor().cast<int>();
     int cell_idx = (cell_pos(0) * num_cells_(1) + cell_pos(1)) * num_cells_(2) + cell_pos(2);
     cells_.at(static_cast<size_t>(cell_idx)).push_back(lattice_id);
   }
@@ -154,7 +203,7 @@ void Config::UpdateNeighborList(std::vector<double> cutoffs) {
                   // Make sure we're not comparing a point to itself, and don't double-count pairs within the same cell
                   if (lattice_id2 >= lattice_id1) { continue; }
                   // Calculate distance
-                  Vector3d relative_distance_vector = GetRelativeDistanceVectorLattice(lattice_id1, lattice_id2);
+                  Eigen::Vector3d relative_distance_vector = GetRelativeDistanceVectorLattice(lattice_id1, lattice_id2);
                   double cartesian_distance_squared = (basis_ * relative_distance_vector).squaredNorm();
                   // If the distance is less than the cutoff, the points are bonded
                   for (size_t cutoff_squared_id = 0; cutoff_squared_id < cutoffs_squared.size(); ++cutoff_squared_id) {
@@ -229,26 +278,28 @@ Config Config::ReadCfg(const std::string &filename) {
   fis.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   // "entry_count = 3"
   fis.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  auto basis = Matrix3d{{basis_xx, basis_xy, basis_xz},
-                        {basis_yx, basis_yy, basis_yz},
-                        {basis_zx, basis_zy, basis_zz}};
-  std::vector<Atom> atom_vector;
+  auto basis = Eigen::Matrix3d{{basis_xx, basis_xy, basis_xz},
+                               {basis_yx, basis_yy, basis_yz},
+                               {basis_zx, basis_zy, basis_zz}};
+  std::vector<Element> atom_vector;
   atom_vector.reserve(num_atoms);
-  Matrix3Xd relative_position_matrix = Matrix3Xd(3, num_atoms);
+  Eigen::Matrix3Xd relative_position_matrix(3, num_atoms);
 
   double mass;
   std::string type;
-  Vector3d relative_position;
+  Eigen::Vector3d relative_position;
 
   std::vector<std::vector<size_t> > first_neighbors_adjacency_list,
       second_neighbors_adjacency_list, third_neighbors_adjacency_list;
 
   for (size_t id = 0; id < num_atoms; ++id) {
     fis >> mass >> type >> relative_position(0) >> relative_position(1) >> relative_position(2);
-    atom_vector.emplace_back(id, type);
+    atom_vector.emplace_back(type);
     relative_position_matrix.col(static_cast<int>(id)) = relative_position;
   }
-  return Config{basis, relative_position_matrix, atom_vector};
+  Config config_out = Config{basis, relative_position_matrix, atom_vector};
+  config_out.ReassignLattice();
+  return config_out;
 }
 
 Config Config::ReadPoscar(const std::string &filename) {
@@ -267,7 +318,7 @@ Config Config::ReadPoscar(const std::string &filename) {
   fis.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // #comment
   double scale;
   fis >> scale; // scale factor, usually which is 1.0
-  Matrix3d basis;
+  Eigen::Matrix3d basis;
   fis >> basis(0, 0) >> basis(0, 1) >> basis(0, 2); // lattice vector a
   fis >> basis(1, 0) >> basis(1, 1) >> basis(1, 2); // lattice vector b
   fis >> basis(2, 0) >> basis(2, 1) >> basis(2, 2); // lattice vector c
@@ -291,9 +342,9 @@ Config Config::ReadPoscar(const std::string &filename) {
   bool relative_option =
       buffer[0] != 'C' && buffer[0] != 'c' && buffer[0] != 'K' && buffer[0] != 'k';
 
-  std::vector<Atom> atom_vector;
+  std::vector<Element> atom_vector;
   atom_vector.reserve(num_atoms);
-  Matrix3Xd relative_position_matrix = Matrix3Xd(3, num_atoms);
+  Eigen::Matrix3Xd relative_position_matrix(3, num_atoms);
 
   size_t id_count = 0;
   double position_X, position_Y, position_Z;
@@ -303,20 +354,24 @@ Config Config::ReadPoscar(const std::string &filename) {
       fis.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       if (relative_option) {
         relative_position_matrix.col(static_cast<int>(id_count)) =
-            Vector3d(position_X, position_Y, position_Z);
+            Eigen::Vector3d(position_X, position_Y, position_Z);
       } else {
         relative_position_matrix.col(static_cast<int>(id_count)) =
-            inverse_basis * Vector3d(position_X, position_Y, position_Z);
+            inverse_basis * Eigen::Vector3d(position_X, position_Y, position_Z);
       }
-      atom_vector.emplace_back(id_count, element_symbol);
+      atom_vector.emplace_back(element_symbol);
       ++id_count;
     }
   }
-  return Config{basis, relative_position_matrix, atom_vector};
+  Config config_out = Config{basis, relative_position_matrix, atom_vector};
+  config_out.ReassignLattice();
+  return config_out;
 }
+
 void Config::WriteConfig(const std::string &filename) const {
   WriteConfigExtended(filename, {});
 }
+
 void Config::WriteConfigExtended(
     const std::string &filename,
     const std::map<std::string, std::vector<double>> &auxiliary_lists) const {
@@ -352,7 +407,7 @@ void Config::WriteConfigExtended(
   for (size_t it = 0; it < atom_vector_.size(); ++it) {
     const auto &atom = atom_vector_[it];
     const auto &relative_position
-        = relative_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(atom.GetId())));
+        = relative_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(it)));
     fos << atom.GetMass() << '\n'
         << atom.GetElementString() << '\n'
         << relative_position.transpose().format(fmt);
@@ -362,6 +417,7 @@ void Config::WriteConfigExtended(
     fos << std::endl;
   }
 }
+
 void Config::WriteXyzExtended(const std::string &filename,
                               const std::map<std::string, VectorVariant> &auxiliary_lists,
                               const std::map<std::string, ValueVariant> &global_list) const {
@@ -394,7 +450,7 @@ void Config::WriteXyzExtended(const std::string &filename,
       fos << "R:1";
     } else if (ret.type() == typeid(std::string)) {
       fos << "S:1";
-    } else if (ret.type() == typeid(Vector3d) && std::any_cast<Vector3d>(ret).size() == 3) {
+    } else if (ret.type() == typeid(Eigen::Vector3d) && std::any_cast<Eigen::Vector3d>(ret).size() == 3) {
       fos << "R:3";
     } else {
       throw std::runtime_error("Unsupported type");
@@ -404,8 +460,7 @@ void Config::WriteXyzExtended(const std::string &filename,
 
   for (size_t it = 0; it < atom_vector_.size(); ++it) {
     const auto &atom = atom_vector_[it];
-    const auto &cartesian_position
-        = cartesian_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(atom.GetId())));
+    const auto &cartesian_position = cartesian_position_matrix_.col(static_cast<int>(atom_to_lattice_hashmap_.at(it)));
     fos << atom.GetElementString() << ' '
         << cartesian_position.transpose().format(fmt) << ' ';
 
@@ -420,8 +475,8 @@ void Config::WriteXyzExtended(const std::string &filename,
         fos << std::any_cast<double>(ret) << ' ';
       } else if (ret.type() == typeid(std::string)) {
         fos << std::any_cast<std::string>(ret) << ' ';
-      } else if (ret.type() == typeid(Vector3d)) {
-        fos << std::any_cast<Vector3d>(ret).format(fmt) << ' ';
+      } else if (ret.type() == typeid(Eigen::Vector3d)) {
+        fos << std::any_cast<Eigen::Vector3d>(ret).format(fmt) << ' ';
       } else {
         throw std::runtime_error("Unsupported type");
       }
