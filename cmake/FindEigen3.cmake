b36f11c311c39cd764b09c254863238b96bfbf60
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-12-03 19:56:53 -0500

    Revert "Update FindEigen3.cmake"
    This reverts commit 44bed490f1da53a447b232a4988daaf305a12908.

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -34,23 +34,27 @@ if (NOT Eigen3_FIND_VERSION)
     if (NOT Eigen3_FIND_VERSION_PATCH)
         set(Eigen3_FIND_VERSION_PATCH 0)
     endif ()
+
     set(Eigen3_FIND_VERSION "${Eigen3_FIND_VERSION_MAJOR}.${Eigen3_FIND_VERSION_MINOR}.${Eigen3_FIND_VERSION_PATCH}")
 endif ()
 
 macro(_eigen3_check_version)
     file(READ "${EIGEN3_INCLUDE_DIRS}/Eigen/src/Core/util/Macros.h" _eigen3_version_header)
+
     string(REGEX MATCH "define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)" _eigen3_world_version_match "${_eigen3_version_header}")
     set(EIGEN3_WORLD_VERSION "${CMAKE_MATCH_1}")
     string(REGEX MATCH "define[ \t]+EIGEN_MAJOR_VERSION[ \t]+([0-9]+)" _eigen3_major_version_match "${_eigen3_version_header}")
     set(EIGEN3_MAJOR_VERSION "${CMAKE_MATCH_1}")
     string(REGEX MATCH "define[ \t]+EIGEN_MINOR_VERSION[ \t]+([0-9]+)" _eigen3_minor_version_match "${_eigen3_version_header}")
     set(EIGEN3_MINOR_VERSION "${CMAKE_MATCH_1}")
+
     set(EIGEN3_VERSION ${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.${EIGEN3_MINOR_VERSION})
     if (${EIGEN3_VERSION} VERSION_LESS ${Eigen3_FIND_VERSION})
         set(EIGEN3_VERSION_OK FALSE)
     else ()
         set(EIGEN3_VERSION_OK TRUE)
     endif ()
+
     if (NOT EIGEN3_VERSION_OK)
         message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIRS}, "
                 "but at least version ${Eigen3_FIND_VERSION} is required")
@@ -83,7 +87,7 @@ else ()
                   ~/.local/include/eigen3
                   ~/.local/include/
                   PATH_SUFFIXES eigen3 eigen
-        )
+                  )
     endif ()
     if (EIGEN3_INCLUDE_DIRS)
         _eigen3_check_version()
@@ -91,6 +95,7 @@ else ()
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(Eigen3 DEFAULT_MSG EIGEN3_INCLUDE_DIRS EIGEN3_VERSION_OK)
     mark_as_advanced(EIGEN3_INCLUDE_DIRS)
+
 endif ()
 
 if (EIGEN3_FOUND AND NOT TARGET Eigen3::Eigen)
