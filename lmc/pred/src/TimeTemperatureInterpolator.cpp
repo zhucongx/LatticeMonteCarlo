dd5393533d8f693a10abe6165d94f25fb8a9fce0
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-12-06 13:06:38 -0500

    Update TimeTemperatureInterpolator.cpp

--- lmc/pred/src/TimeTemperatureInterpolator.cpp
+++ lmc/pred/src/TimeTemperatureInterpolator.cpp
@@ -1,41 +1,36 @@
 #include "TimeTemperatureInterpolator.h"
 
-#include <limits>
-#include <fstream>
 #include <algorithm>
+#include <fstream>
+#include <limits>
 #include <stdexcept>
 #include <utility>
 
 namespace pred {
-TimeTemperatureInterpolator::TimeTemperatureInterpolator(
-    const std::string &time_temperature_filename) {
-  if (time_temperature_filename.empty()) {
-    return;
-  }
+TimeTemperatureInterpolator::TimeTemperatureInterpolator(const std::string &time_temperature_filename)
+{
+  if (time_temperature_filename.empty()) { return; }
   std::ifstream ifs(time_temperature_filename, std::ifstream::in);
   if (!ifs.is_open()) {
     throw std::runtime_error("Cannot open " + time_temperature_filename);
     return;
   }
   double time, temperature;
-  while (ifs.peek() != '0') {
-    ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  }
+  while (ifs.peek() != '0') { ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); }
   while (ifs >> time >> temperature) {
-    if (ifs.eof() || ifs.bad()) {
-      break;
-    }
+    if (ifs.eof() || ifs.bad()) { break; }
     points_.emplace_back(time, temperature);
     ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   }
   SortPoints();
 }
-TimeTemperatureInterpolator::TimeTemperatureInterpolator(
-    const std::vector<std::pair<double, double>> &points)
-    : points_(points) {
+TimeTemperatureInterpolator::TimeTemperatureInterpolator(const std::vector<std::pair<double, double>> &points)
+    : points_(points)
+{
   SortPoints();
 }
-void TimeTemperatureInterpolator::SortPoints() {
+void TimeTemperatureInterpolator::SortPoints()
+{
   //Defensive programming. Assume the caller has not sorted the table ascending order
   std::sort(points_.begin(), points_.end());
   //Ensure that no 2 adjacent x values are equal, lest we try to divide by zero when we interpolate.
@@ -47,21 +42,18 @@ void TimeTemperatureInterpolator::SortPoints() {
     }
   }
 }
-double TimeTemperatureInterpolator::GetTemperature(double time) const {
+double TimeTemperatureInterpolator::GetTemperature(double time) const
+{
   //Define a lambda that returns true if the time value of a point pair is < the caller's time value
-  auto less_than =
-      [](const std::pair<double, double> &point, double x) { return point.first < x; };
+  auto less_than = [](const std::pair<double, double> &point, double x) {
+    return point.first < x;
+  };
   //Find the first table entry whose value is >= caller's time value
-  auto iter =
-      std::lower_bound(points_.cbegin(), points_.cend(), time, less_than);
+  auto iter = std::lower_bound(points_.cbegin(), points_.cend(), time, less_than);
   //If the caller's X value is greater than the largest X value in the table, we can't interpolate.
-  if (iter == points_.cend()) {
-    return (points_.cend() - 1)->second;
-  }
+  if (iter == points_.cend()) { return (points_.cend() - 1)->second; }
   //If the caller's X value is less than the smallest X value in the table, we can't interpolate.
-  if (iter == points_.cbegin() and time <= points_.cbegin()->first) {
-    return points_.cbegin()->second;
-  }
+  if (iter == points_.cbegin() and time <= points_.cbegin()->first) { return points_.cbegin()->second; }
   //We can interpolate!
   double upper_x{iter->first};
   double upper_y{iter->second};
@@ -73,4 +65,4 @@ double TimeTemperatureInterpolator::GetTemperature(double time) const {
   return lower_y + ((time - lower_x) / deltaX) * deltaY;
 }
 
-} // pred
+}    // namespace pred
