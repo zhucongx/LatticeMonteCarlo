59a22368ba8603499886946d8636c232002c216c
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-12-11 12:14:31 -0500

    Update EnergyChangePredictorSite.h

--- lmc/pred/include/EnergyChangePredictorSite.h
+++ lmc/pred/include/EnergyChangePredictorSite.h
@@ -1,37 +1,34 @@
 #ifndef LMC_LMC_PRED_INCLUDE_ENERGYCHANGEPREDICTORSITE_H_
 #define LMC_LMC_PRED_INCLUDE_ENERGYCHANGEPREDICTORSITE_H_
-#include <string>
-#include <unordered_set>
-#include <unordered_map>
-#include <boost/functional/hash.hpp>
 #include "Config.h"
-#include "LatticeCluster.hpp"
 #include "ElementCluster.hpp"
 #include "EnergyUtility.h"
+#include "LatticeCluster.hpp"
+#include <boost/functional/hash.hpp>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace pred {
 
 class EnergyChangePredictorSite {
-  public:
-    EnergyChangePredictorSite(const std::string &predictor_filename,
-                              const cfg::Config &reference_config,
-                              std::set<Element> element_set);
-    virtual ~EnergyChangePredictorSite();
-    [[nodiscard]] double GetDeFromAtomIdSite(
-        const cfg::Config &config, size_t atom_id, Element new_element) const;
-  private:
-    [[nodiscard]] double GetDeFromLatticeIdSite(
-        const cfg::Config &config, size_t lattice_id, Element new_element) const;
-    const std::set<Element> element_set_;
-    std::vector<double> base_theta_{};
-    std::unordered_map<cfg::ElementCluster, size_t, boost::hash<cfg::ElementCluster> >
-        initialized_cluster_hashmap_{};
+ public:
+  EnergyChangePredictorSite(const std::string &predictor_filename,
+                            const cfg::Config &reference_config,
+                            std::set<Element> element_set);
+  virtual ~EnergyChangePredictorSite();
+  [[nodiscard]] double GetDeFromAtomIdSite(const cfg::Config &config, size_t atom_id, Element new_element) const;
+  [[nodiscard]] double GetDeFromLatticeIdSite(const cfg::Config &config, size_t lattice_id, Element new_element) const;
 
-    std::unordered_map<size_t, std::vector<std::vector<std::vector<size_t> > > >
-        site_neighbors_hashmap_{};
+ private:
+  const std::set<Element> element_set_;
+  std::vector<double> base_theta_{};
+  std::unordered_map<cfg::ElementCluster, size_t, boost::hash<cfg::ElementCluster>> initialized_cluster_hashmap_{};
+
+  std::unordered_map<size_t, std::vector<std::vector<std::vector<size_t>>>> site_neighbors_hashmap_{};
 };
 
-} // pred
+}    // namespace pred
 
 
-#endif //LMC_LMC_PRED_INCLUDE_ENERGYCHANGEPREDICTORSITE_H_
+#endif    //LMC_LMC_PRED_INCLUDE_ENERGYCHANGEPREDICTORSITE_H_
