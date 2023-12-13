881094cc50d6006b5e291c65a9754c58aee14875
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-12-13 00:49:42 -0500

    Update CanonicalMcAbstract.h

--- lmc/mc/include/CanonicalMcAbstract.h
+++ lmc/mc/include/CanonicalMcAbstract.h
@@ -1,40 +1,38 @@
 #ifndef LMC_LMC_MC_INCLUDE_CANONICALMCABSTRACT_H_
 #define LMC_LMC_MC_INCLUDE_CANONICALMCABSTRACT_H_
-#include <random>
+#include "EnergyChangePredictorPairAll.h"
+#include "McAbstract.h"
+
 #include <mpi.h>
 #include <omp.h>
-#include "McAbstract.h"
-#include "EnergyChangePredictorPairAll.h"
+#include <random>
+
 namespace mc {
 
 class CanonicalMcAbstract : public McAbstract {
-  public:
-    CanonicalMcAbstract(cfg::Config config,
-                        unsigned long long int log_dump_steps,
-                        unsigned long long int config_dump_steps,
-                        unsigned long long int maximum_steps,
-                        unsigned long long int thermodynamic_averaging_steps,
-                        unsigned long long int restart_steps,
-                        double restart_energy,
-                        double temperature,
-                        // double initial_temperature,
-                        // double decrement_temperature,
-                        const std::set<Element> &element_set,
-                        const std::string &json_coefficients_filename);
-    void Simulate() override = 0;
-  protected:
-    // void UpdateTemperature();
-    virtual void Dump() const;
-    std::pair<size_t, size_t> GenerateLatticeIdJumpPair();
-    void SelectEvent(const std::pair<size_t, size_t> &lattice_id_jump_pair, double dE);
-    // simulation parameters
-    // const double initial_temperature_;
-    // const double decrement_temperature_;
-    // helpful properties
-    const pred::EnergyChangePredictorPairAll energy_change_predictor_;
-    mutable std::uniform_int_distribution<size_t> atom_index_selector_;
+ public:
+  CanonicalMcAbstract(cfg::Config config,
+                      unsigned long long int log_dump_steps,
+                      unsigned long long int config_dump_steps,
+                      unsigned long long int maximum_steps,
+                      unsigned long long int thermodynamic_averaging_steps,
+                      unsigned long long int restart_steps,
+                      double restart_energy,
+                      double temperature,
+                      const std::set<Element> &element_set,
+                      const std::string &json_coefficients_filename);
+  void Simulate() override = 0;
+
+ protected:
+  virtual void Dump() const;
+  std::pair<size_t, size_t> GenerateLatticeIdJumpPair();
+  void SelectEvent(const std::pair<size_t, size_t> &lattice_id_jump_pair, double dE);
+
+  // helpful properties
+  const pred::EnergyChangePredictorPairAll energy_change_predictor_;
+  mutable std::uniform_int_distribution<size_t> atom_index_selector_;
 };
 
-} // mc
+}    // namespace mc
 
-#endif //LMC_LMC_MC_INCLUDE_CANONICALMCABSTRACT_H_
+#endif    //LMC_LMC_MC_INCLUDE_CANONICALMCABSTRACT_H_
