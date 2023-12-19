49fa176a50056ee580ccfcc4f0c516691cf1f903
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-12-18 22:06:53 -0500

    Update SimulatedAnnealing.h

--- lmc/mc/include/SimulatedAnnealing.h
+++ lmc/mc/include/SimulatedAnnealing.h
@@ -8,7 +8,7 @@
 #include <random>
 
 namespace mc {
-class SimulatedAnnealing : private CanonicalMcAbstract {
+class SimulatedAnnealing : public McAbstract {
  public:
   SimulatedAnnealing(const Factor_t &factors,
                      Element solvent_element,
@@ -21,12 +21,14 @@ class SimulatedAnnealing : private CanonicalMcAbstract {
                      const std::string &json_coefficients_filename);
   void Simulate() override;
  private:
-  void Dump() const override;
+  virtual void Dump() const;
   void UpdateTemperature();
-
+  std::pair<size_t, size_t> GenerateLatticeIdJumpPair();
+  void SelectEvent(const std::pair<size_t, size_t> &lattice_id_jump_pair, double dE);
   // helpful properties
+  const pred::EnergyChangePredictorPairSite energy_change_predictor_;
+  mutable std::uniform_int_distribution<size_t> atom_index_selector_;
   mutable double lowest_energy_;
-//  unsigned long long count_;
   // simulation parameters
   const double initial_temperature_;
   const double decrement_temperature_;
